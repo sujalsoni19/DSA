d37a25e3a3384b60a896cd45adb0cf7e289f9da1
sujalsoni19 <sujal19soni2004@gmail.com>
2026-03-13 20:13:20 +0530

    Rotation count and LC 189

---
+++ BinarySearch/RotationCountinRSA.java
@@ -0,0 +1,30 @@
+public class RotationCountinRSA {
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,4,5,6};
+
+        System.out.println(pivotinRSA(arr) + 1);
+    }
+    static int pivotinRSA(int[] arr){
+        
+        int start = 0;
+        int end = arr.length - 1;
+
+        while(start <= end){
+            int mid = start + (end - start) / 2;
+
+            if( mid < end && arr[mid] > arr[mid+1]){
+                return mid;
+            }
+            if(mid > start && arr[mid] < arr[mid-1]){
+                return mid-1;
+            }
+            if(arr[start] >= arr[mid]){
+                end = mid - 1;
+            }
+            else{
+                start = mid + 1;
+            }
+        }
+        return -1;
+    }
+}
